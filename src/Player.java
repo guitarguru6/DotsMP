44fc7aa77b09a30780321ce0d88cf483a0a0b651
guitarguru6 <guitargurugaming@gmail.com>
2015-05-29 12:32:17 -0500

    0.1.6
    +player now has a minimum size of 10

--- src/Player.java
+++ src/Player.java
@@ -5,34 +5,36 @@ import java.util.Random;
 public class Player {
 	public static double x, y, diameter, radius;
 	public static int minX = 0, minY = 0, maxX = 2400, maxY = 1800;
+	private static double minSize = 10.0;
 	private static Color color;
 	private static Color[] colors;
 
 	public Player(int x, int y, int diameter) {
-		this.x = x;
-		this.y = y;
+		Player.x = x;
+		Player.y = y;
 		setDiameter((double) diameter);
 		setColor();
 	}
-	
+
 	public static void increaseD() {
 		setDiameter(diameter + 1);
 	}
-	
+
 	public static void decreaseD() {
 		setDiameter(diameter - 1);
 	}
-	
+
 	public static void setColor() {
-		colors = new Color[]{Color.GREEN, Color.BLUE, Color.CYAN, Color.MAGENTA, Color.ORANGE, Color.PINK, Color.RED, Color.YELLOW};
+		colors = new Color[] { Color.GREEN, Color.BLUE, Color.CYAN, Color.MAGENTA, Color.ORANGE, Color.PINK, Color.RED, Color.YELLOW };
 		color = colors[new Random().nextInt(colors.length)];
 	}
 
 	public static void setDiameter(Double d) {
+		if(d < minSize) d = minSize;
 		diameter = d;
 		radius = d / 2.0;
 	}
-	
+
 	public void tick() {
 		if (x < minX + radius) {
 			x = minX + radius;
@@ -47,9 +49,9 @@ public class Player {
 			y = maxY - radius;
 		}
 	}
-	
+
 	public void render(Graphics g) {
 		g.setColor(color);
-		g.fillOval((int)(x-radius), (int)(y - radius), (int)diameter, (int)diameter);
+		g.fillOval((int) (x - radius), (int) (y - radius), (int) diameter, (int) diameter);
 	}
 }
